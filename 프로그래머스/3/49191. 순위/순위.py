2cc93e98f7c309bc7cacb18fbb357059cacab753
wnswns <51504673+jy6443@users.noreply.github.com>
2024-03-21 20:47:56 +0900

    [level 3] Title: 순위, Time: 12.55 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,36 @@
+import copy
+def solution(n, results):
+    answer = 0
+    w = {i:[] for i in range(1,n+1)}
+    l = {i:[] for i in range(1,n+1)}
+    for result in results:
+        a,b = map(int,result)
+        w[a] += [b]
+        l[b] += [a]
+    
+    
+    def dfs(k,p):
+        visit = [False] * (n+1)
+        visit[k] = True
+        s = []
+        for i in p[k]:
+            visit[i] = True
+            s.append(i)
+        while s:
+            out = s.pop()
+            for x in p[out]:
+                if not visit[x]:
+                    visit[x] = True
+                    p[k] += [x]
+                    s.append(x)
+        return p
+
+    for k in range(1,n+1):
+        w = dfs(k,w)
+        l = dfs(k,l)
+            
+    for player in range(1,n+1):
+        if len(w[player]) + len(l[player]) == n-1:
+            answer += 1
+    
+    return answer
