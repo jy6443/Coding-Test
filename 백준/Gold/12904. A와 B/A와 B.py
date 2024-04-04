ce09e3abebde7b756dba6721ed6a74d94809b4be
SogCheonBul <51504673+jy6443@users.noreply.github.com>
2024-04-04 22:07:41 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+s = input().strip()
+t = input().strip()
+
+while len(t) > len(s):
+    if t[-1] == 'A':
+        t = t[:-1]
+    else:
+        t = t[:-1]
+        t = t[::-1]
+
+if t == s:
+    print(1)
+else:
+    print(0)
