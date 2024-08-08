b888973e8156afe9adaba13598ddd6f29c5cc07b
SogCheonBul <51504673+jy6443@users.noreply.github.com>
2024-08-08 18:53:41 +0900

    [level 2] Title: 후보키, Time: 10.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,47 +1,48 @@
 import java.util.*;
 class Solution {
+    public int row;
+    public int col;
     public int solution(String[][] relation) {
         int answer = 0;
-        int col = relation[0].length;
-        int row = relation.length;
-        List<List<Integer>> keys = new ArrayList<>(); //열의 조합들을 다 모은 이차원배열
-        for(int cnt=1; cnt<=col; cnt++) { //몇개의 속성(열)의 조합
-            List<Integer> now = new ArrayList<>(); //이번턴의 열 조합
-            dfs(cnt,0,col,now,keys,relation);
+        row = relation.length;
+        col = relation[0].length;
+        List<List<Integer>> keys = new ArrayList<>();
+        for(int cnt=1; cnt<=col; cnt++){ // i=1
+            List<Integer> key = new ArrayList<>();
+            boolean[] visit = new boolean[col];
+            find(key,cnt,keys,0,relation,visit);
         }
-        answer = keys.size();
-        return answer;
+        return keys.size();
     }
-    
-    public void dfs(int cnt, int start, int end, List<Integer> now, List<List<Integer>> keys, String[][] relation) {
-        if(now.size() == cnt && isKey(now, relation)) {
-            for (List<Integer> one : keys) {
-                if(now.containsAll(one)){
+    public void find(List<Integer> key, int cnt, List<List<Integer>> keys, int idx,String[][] relation,boolean[] visit){
+        if(key.size() == cnt && uniq(key,relation)){
+            for(List<Integer> min : keys){
+                if(key.containsAll(min)){
                     return;
                 }
             }
-            keys.add(new ArrayList<>(now));
+            keys.add(new ArrayList<>(key));
             return;
         }
-        for (int i=start; i<end; i++) {
-            now.add(i);
-            dfs(cnt,i+1,end,now,keys,relation);
-            now.remove(now.size()-1);
+        for(int i=idx; i<col; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                key.add(i);
+                find(key,cnt,keys,i,relation,visit);
+                visit[i] = false;
+                key.remove(key.size()-1);
+            }
         }
     }
-    public boolean isKey(List<Integer> now, String[][] relation) {
+    public boolean uniq(List<Integer> key, String[][] relation){
         Set<String> set = new HashSet<>();
-        for (String[] row : relation) {
-            StringBuilder sb = new StringBuilder();
-            for (int idx : now) {
-                sb.append(row[idx]);
+        for(String[] rel : relation){
+            String check = "";
+            for(int id : key){
+                check += rel[id];
             }
-            set.add(sb.toString());
+            set.add(check);
         }
-        if(relation.length == set.size()) {
-            return true;
-        } else {
-            return false;
-        }
+        return relation.length == set.size();
     }
 }
