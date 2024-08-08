f9766318ae40ce5bb9c8eb07a610b83f0286d3af
SogCheonBul <51504673+jy6443@users.noreply.github.com>
2024-08-08 22:59:02 +0900

    [level 2] Title: N-Queen, Time: 222.53 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,33 +1,40 @@
 class Solution {
-    public int answer;
     public int solution(int n) {
-        answer = 0;
-        int[][] visit = new int[n][n];
-        dfs(0,visit,n);
+        boolean[][] visit = new boolean[n][n];
+        int answer = queen(visit,0,n);
         return answer;
     }
-    public void dfs(int s, int[][] visit, int n) {
-        if(s == n) {
-            answer++;
-            return;
+    public int queen(boolean[][] visit, int row, int n){
+        if(row == n){
+            return 1;
         }
-        for(int i=0; i<n; i++) {
-            if(visit[s][i] == 0){
-                queen(visit,s,i,n,1);
-                dfs(s+1,visit,n);
-                queen(visit,s,i,n,-1);
+        int cnt = 0;
+        for(int col=0; col<n; col++){
+            if(check(row,col,n,visit)){
+                visit[row][col] = true;
+                cnt += queen(visit,row+1,n);
+                visit[row][col] = false;
             }
         }
+        return cnt;
     }
-    public void queen(int[][] visit, int row, int col, int n, int flag) {
-        for(int i=0; i<n; i++) {
-            visit[i][col]+=flag;
-            for(int j=0; j<n; j++) {
-                if(col-row == j-i || col+row == i+j) {
-                    visit[i][j]+=flag;
-                }
+    
+    public boolean check(int row, int col, int n, boolean[][] visit){
+        for(int i=0; i<row; i++){
+            if(visit[i][col]){
+                return false;
             }
         }
-        visit[row][col]-=flag;
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--){
+            if(visit[i][j]){
+                return false;
+            }       
+        }
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--,j++){
+            if(visit[i][j]){
+                return false;
+            } 
+        }
+        return true;
     }
 }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 94.2 MB, 시간: 376.43 ms
+메모리: 74.4 MB, 시간: 222.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 19일 18:04:42
+2024년 08월 08일 22:58:59
 
 ### 문제 설명
 
