3d4c9417e8380052f15f41ba5af07344220be5e4
SogCheonBul <51504673+jy6443@users.noreply.github.com>
2024-08-08 17:45:06 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,23 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visit = new boolean[n];
-        // for(int i=0; i<n; i++) {
-        //     computer[i][i] = 0;
-        // }
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                if(!visit[j] && computers[i][j] == 1) {
-                    visit[j] = true;
-                    dfs(j,computers,visit);
-                    answer++;
-                }
+        boolean[] visit = new boolean[n]; 
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(i, computers, visit); 
+                answer++;
             }
         }
+        
         return answer;
     }
-    public void dfs(int j, int[][] computers, boolean[] visit) {
-        for(int k=0; k<computers.length; k++) {
-            if(!visit[k] && computers[j][k] == 1) {
-                visit[k] = true;
-                dfs(k,computers,visit);
+    
+    public void dfs(int node, int[][] computers, boolean[] visit) {
+        visit[node] = true;
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1 && !visit[i]) {
+                dfs(i, computers, visit);
             }
         }
     }
